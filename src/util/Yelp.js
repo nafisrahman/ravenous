be3b4c3b83e0aaa289d945cf3442b6ec9be60320
nafisrahman <nafisrahman797@gmail.com>
2020-07-28 21:23:46 -0400

    initial project

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = "GJGpWYK5dWSqnLFHepbizJ4yPW0nBHaW1K89J_qlgvAAqbDsF1LcEi7cbYHttsmjlgQKBoDuY-tvfISQ8g_VP8E_J6fMyN4lrsSKnLh_-wf7GHunVs9rFeKahkruXnYx"
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
+
+
